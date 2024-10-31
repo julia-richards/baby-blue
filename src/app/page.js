2f336987c834bfc62609a438614e649e90f9ac94
juliarichards <julia.richards@advancedagrilytics.com>
2024-10-30 21:16:13 -0600

    Remove styled components

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,10 @@
 'use client'
-import { createGlobalStyle } from 'styled-components'
 
 // import { useState, useEffect } from 'react'
 // import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 // import { Facebook, Instagram, Twitter } from "lucide-react"
 
-const GlobalStyle = createGlobalStyle`
-.bg-stripes {
-  background-image: repeating-linear-gradient(
-    to right,
-    #C5E7F7,
-    #C5E7F7 40px,
-    #F7F3E9 40px,
-    #F7F3E9 80px
-  );
-}
-`
+
 
 export default function Home() {
   // const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
@@ -46,7 +35,16 @@ export default function Home() {
 
   return (
     <>
-      <GlobalStyle />
+      <style>{
+        `.bg-stripes {
+          background-image: repeating-linear-gradient(
+            to right,
+            #C5E7F7,
+            #C5E7F7 40px,
+            #F7F3E9 40px,
+            #F7F3E9 80px
+          );`
+        }</style>
     <div className="min-h-screen bg-stripes flex flex-col items-center justify-center p-4">
       <div className="rounded-lg p-8 max-w-md w-full" style={{background: '#F7F3E9'}}>
         <header className="text-center mb-8">
